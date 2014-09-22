64b53226bd15d72c4bb374dbc712cac61b6f062e
kevinstyle <kevinstyle@gmail.com>
2014-09-22 13:26:16 +0800

    Initial Commit

---
+++ qrlink.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Wgame | PowerStrike</title>
+</head>
+
+<body>
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+	_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+	_gaq.push(['_setAccount', 'UA-38257563-1']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+<script type="text/javascript">
+	var useragent = navigator.userAgent;
+	useragent = useragent.toLowerCase();
+
+	if (useragent.indexOf('iphone') != -1 || useragent.indexOf('ipad') != -1 || useragent.indexOf('ipod') != -1 ) {
+		window.location = 'https://itunes.apple.com/tw/app/huo-li-quan-kai-su-ming-dui-jue/id642801181?mt=8';
+	}else if(useragent.indexOf('android') != -1){
+		window.location = 'market://details?id=com.wgame.PowerStrike';
+	}else if(useragent.indexOf('blackberry') != -1 || useragent.indexOf('samsung') != -1 || useragent.indexOf('nokia') != -1 || useragent.indexOf('windows ce') != -1 || useragent.indexOf('sonyericsson') != -1 || useragent.indexOf('webos') != -1 || useragent.indexOf('wap') != -1 || useragent.indexOf('motor') != -1 || useragent.indexOf('symbian') != -1 || useragent.indexOf('symbianos') != -1 ){
+		window.location = 'market://details?id=com.wgame.PowerStrike';
+	}else{
+		window.location = 'http://www.wgame.com.tw/';
+	}
+</script>
+</body>
+</html>
